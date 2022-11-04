350a05112b9d98ad68eda94ec09218cfe4926879
Ken Poon <kenpoon94@gmail.com>
2022-11-04 09:00:45 +0800

    Added utils folder

---
+++ lesson8-project/utils/General.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const convertToBytes32Array = (array: string[]) => {
+  const bytes32Array = array.map((x: string) =>
+    ethers.utils.formatBytes32String(x)
+  );
+  return bytes32Array;
+};
+
+export const getSigner = () => {
+  const provider = ethers.getDefaultProvider("goerli", {
+    etherscan: process.env.ETHERSCAN_API_KEY,
+    alchemy: process.env.ALCHEMY_API_KEY,
+  });
+  const wallet = ethers.Wallet.fromMnemonic(
+    process.env.MNEMONIC ?? "",
+    process.env.ACCOUNT_PATH ?? ""
+  );
+  return wallet.connect(provider);
+};
