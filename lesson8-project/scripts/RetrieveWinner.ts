ae2fa5efdf12140da1f3e938a372d6e23e16a82e
Ken Poon <kenpoon94@gmail.com>
2022-11-05 16:38:58 +0800

    Added retrieveWinner script

--- lesson8-project/scripts/RetrieveWinner.ts
+++ lesson8-project/scripts/RetrieveWinner.ts
@@ -1,24 +1,23 @@
 import { Ballot__factory } from "../typechain-types";
 import * as dotenv from "dotenv";
 import { getSigner } from "../utils/General";
+import { ethers } from "hardhat";
 dotenv.config();
 
 async function main() {
+  console.log("Retrieving winning proposal");
+
   const contractAddress = process.argv[2];
-  const targetAddress = process.argv[3];
-  console.log(`Give right to vote to ${targetAddress}`);
 
   const signer = getSigner();
-  const balance = await signer.getBalance();
 
-  if (balance.eq(0))
-    throw new Error("Not enough balance to interact with contract");
   const ballotContractFactory = new Ballot__factory(signer);
   const ballotContract = ballotContractFactory.attach(contractAddress);
-  const tx = await ballotContract.giveRightToVote(targetAddress);
-  await tx.wait();
-
-  console.log(`Right to vote has been given to ${targetAddress}`);
+  let winner: string = "";
+  await ballotContract
+    .winnerName()
+    .then((x) => (winner = ethers.utils.parseBytes32String(x)));
+  console.log(`Current winning proposal is ${winner}`);
 }
 
 main().catch((error) => {
