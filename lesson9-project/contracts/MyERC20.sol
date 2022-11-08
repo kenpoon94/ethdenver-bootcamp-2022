db3f62d3aa186d7b459e2c990e3ff53f438da167
Ken Poon <kenpoon94@gmail.com>
2022-11-08 20:49:21 +0800

    Grant role test

--- lesson9-project/contracts/MyERC20.sol
+++ lesson9-project/contracts/MyERC20.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+contract MyToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("MyToken", "MTK") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
 }
--- lesson9-project/scripts/TestERC20.ts
+++ lesson9-project/scripts/TestERC20.ts
@@ -1,11 +1,30 @@
+import { keccak256 } from "ethers/lib/utils";
 import { ethers } from "hardhat";
-import { token } from "../typechain-types/@openzeppelin/contracts";
+
+const MINTER_ROLE_CODE =
+  "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6";
 
 async function main() {
+  const accounts = await ethers.getSigners();
   const tokenContractFactory = await ethers.getContractFactory("MyToken");
   const tokenContract = await tokenContractFactory.deploy();
   await tokenContract.deployed();
   console.log(`Contract deployed at  ${tokenContract.address}`);
+  // Give role
+  const roleTx = await tokenContract.grantRole(
+    MINTER_ROLE_CODE,
+    accounts[2].address
+  );
+  await roleTx.wait();
+  // Minting Tokens
+  const mintTx = await tokenContract
+    .connect(accounts[2])
+    .mint(accounts[0].address, 2);
+  await mintTx.wait();
+
+  // Sending a transaction
+  const tx = await tokenContract.transfer(accounts[1].address, 1);
+  await tx.wait();
   const [name, symbol, decimals, totalSupply] = await Promise.all([
     tokenContract.name(),
     tokenContract.symbol(),
@@ -18,6 +37,10 @@ async function main() {
     decimals,
     totalSupply,
   });
+  const myBalance0 = await tokenContract.balanceOf(accounts[0].address);
+  const myBalance1 = await tokenContract.balanceOf(accounts[1].address);
+  console.log(`Account balance 0 is ${myBalance0.toString()}`);
+  console.log(`Account balance 1 is ${myBalance1.toString()}`);
 }
 
 main().catch((err) => {
