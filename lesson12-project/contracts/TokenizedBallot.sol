a3c6d9b3252d6940e6c1bd9eb3e69c61b4f9236a
Ken Poon <kenpoon94@gmail.com>
2022-11-11 08:31:07 +0800

    Added TokenizedBallot.ts

---
+++ lesson12-project/contracts/TokenizedBallot.sol
@@ -0,0 +1,60 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IMyToken {
+    function getPastVotes(address, uint256) external view returns (uint256);
+}
+
+contract TokenizedBallot {
+    struct Proposal {
+        bytes32 name;
+        uint256 voteCount;
+    }
+
+    Proposal[] public proposals;
+    IMyToken public tokenContract;
+    uint256 public targetBlockNumber;
+
+    mapping(address => uint256) public votingPowerSpent;
+
+    constructor(
+        bytes32[] memory proposalNames,
+        address _tokenContract,
+        uint256 _targetBlockNumber
+    ) {
+        targetBlockNumber = _targetBlockNumber;
+        tokenContract = IMyToken(_tokenContract);
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+        }
+    }
+
+    function vote(uint256 proposal, uint256 amount) external {
+        require(
+            votingPower(msg.sender) >= amount,
+            "TokenizedBallot: trying to vote more than allowed"
+        );
+
+        votingPowerSpent[msg.sender] += amount;
+        proposals[proposal].voteCount += amount;
+    }
+
+    function votingPower(address account) public view returns (uint256) {
+        return
+            tokenContract.getPastVotes(account, targetBlockNumber) -
+            votingPowerSpent[account];
+    }
+
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
