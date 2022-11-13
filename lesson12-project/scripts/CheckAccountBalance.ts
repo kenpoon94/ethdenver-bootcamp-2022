f28d45c0f1f131578fc323695ecf3e5c9955b009
Ken Poon <kenpoon94@gmail.com>
2022-11-13 14:26:43 +0800

    Check account balance script

--- lesson12-project/scripts/CheckAccountBalance.ts
+++ lesson12-project/scripts/CheckAccountBalance.ts
@@ -1,13 +1,25 @@
 import { getSigner } from "../../utils/General";
 
 async function main() {
-  const signer1 = getSigner(process.env.ACCOUNT_PATH);
+  const signer1 = getSigner(process.env.META_1);
   await signer1.getBalance().then((balance) => {
     console.log(`Signer 1 balance : ${balance.toString()}`);
   });
-  const signer2 = getSigner(process.env.SECONDARY_ACCOUNT_PATH);
+  const signer2 = getSigner(process.env.META_2);
   await signer2.getBalance().then((balance) => {
     console.log(`Signer 2 balance : ${balance.toString()}`);
+  });
+  const signer3 = getSigner(process.env.META_3);
+  await signer3.getBalance().then((balance) => {
+    console.log(`Signer 3 balance : ${balance.toString()}`);
+  });
+  const signer4 = getSigner(process.env.META_4);
+  await signer4.getBalance().then((balance) => {
+    console.log(`Signer 4 balance : ${balance.toString()}`);
+  });
+  const signer5 = getSigner(process.env.META_5);
+  await signer5.getBalance().then((balance) => {
+    console.log(`Signer 5 balance : ${balance.toString()}`);
   });
 }
 
