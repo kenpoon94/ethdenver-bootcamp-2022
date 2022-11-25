354d2de5edac2c63d4f3bf5768c01f61a0bd60cb
Ken Poon <kenpoon94@gmail.com>
2022-11-25 12:37:44 +0800

    Rename folder and add lesson20 frontend

---
+++ lesson20-project-frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import VotingDashboard from "./VotingDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <VotingDashboard />
+    </div>
+  );
+}
+
+export default App;
